f083cf247071fc5e25b4f786d23df0c635d28ad3
Thiago Tavares <tvttavares@gmail.com>
2021-01-21 20:40:39 -0400

    Camada de serviços

--- kafka-idempotencia-e-garantias/ecommerce/service-email/src/main/java/br/com/alura/ecommerce/EmailService.java
+++ kafka-idempotencia-e-garantias/ecommerce/service-email/src/main/java/br/com/alura/ecommerce/EmailService.java
@@ -1,24 +1,24 @@
 package br.com.alura.ecommerce;
 
-import br.com.alura.ecommerce.consumer.KafkaService;
+import br.com.alura.ecommerce.consumer.ConsumerService;
+import br.com.alura.ecommerce.consumer.ServiceRunner;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 
-import java.util.Map;
-import java.util.concurrent.ExecutionException;
+public class EmailService implements ConsumerService<String> {
 
-public class EmailService {
-
-    public static void main(String[] args) throws ExecutionException, InterruptedException {
-        var emailService = new EmailService();
-        try (var service = new KafkaService(EmailService.class.getSimpleName(),
-                "ECOMMERCE_SEND_EMAIL",
-                emailService::parse,
-                Map.of())) {
-            service.run();
-        }
+    public static void main(String[] args) {
+        new ServiceRunner(EmailService::new).start(5);
     }
 
-    private void parse(ConsumerRecord<String, Message<String>> record) {
+    public String getConsumerGroup() {
+        return EmailService.class.getSimpleName();
+    }
+
+    public String getTopic() {
+        return "ECOMMERCE_SEND_EMAIL";
+    }
+
+    public void parse(ConsumerRecord<String, Message<String>> record) {
         System.out.println("------------------------------------------");
         System.out.println("Send email");
         System.out.println(record.key());
